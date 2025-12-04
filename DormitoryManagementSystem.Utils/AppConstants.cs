d25dc164df92642fa4c6b1902c763c44b3b30c10
HuyMinh <nguyenminhhuy595@gmail.com>
2025-12-04 21:19:06 +0700

    THêm constasnt

---
+++ DormitoryManagementSystem.Utils/AppConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace DormitoryManagementSystem.Utils
+{
+    public static class AppConstants
+    {
+        public static class Role
+        {
+            public const string Admin = "Admin";
+            public const string Student = "Student";
+        }
+        public static class RoomStatus
+        {
+            public const string Active = "Active";
+            public const string Inactive = "Inactive";
+            public const string Maintenance = "Maintenance";
+        }
+        public static class ContractStatus
+        {
+            public const string Active = "Active";
+            public const string Pending = "Pending";
+            public const string Terminated = "Terminated";
+            public const string Expired = "Expired";
+        }
+        public static class PaymentStatus
+        {
+            public const string Paid = "Paid";
+            public const string Unpaid = "Unpaid";
+            public const string Late = "Late";
+        }
+    }
+}
